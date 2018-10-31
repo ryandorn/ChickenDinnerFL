792a4c8c0b78438a154f99f792c33dfbfa91d6c2
ryandorn <43752751+ryandorn@users.noreply.github.com>
2018-10-31 10:56:30 +0000

    Added SqlExec class to framework and requisite package
    Added SQLExecutor class to run sql or also query db. Added requisite data.sqlclient package to solution. NEED: Database name after setup.

---
+++ ChickenDinnerFL/Framework/SQL/SQLExecutor.cs
@@ -0,0 +1,106 @@
+﻿using ChickenDinnerFL.Framework.Common;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenDinnerFL.Framework.SQL
+{
+    public class SQLExecutor
+    {
+        private static string connectionString = "@Server=sogetidb.cwffvv6nyyql.us-west-2.rds.amazonaws.com;Databse=TBD;Uid=Hackathon;Pwd=Sogeti999;Connection Timeout=120";
+
+        internal static void executeSQL(string query)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                SqlDataReader reader;
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+
+                if (query.ToLower().Contains("exec"))
+                {
+                    cmd.CommandTimeout = 600;
+                }
+
+                conn.Open();
+
+                reader = cmd.ExecuteReader();
+
+                do
+                {
+                    int count = reader.FieldCount;
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            query = reader.GetValue(i).ToString();
+                        }
+                    }
+                } while (reader.NextResult());
+                cmd.CommandTimeout = 120;
+
+                conn.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message + ": " + query);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        internal static string executeSQL_ReturnResult(string query)
+        {
+            string results = "";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+
+                conn.Open();
+
+                reader = cmd.ExecuteReader();
+
+                do
+                {
+                    int count = reader.FieldCount;
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            results = reader.GetValue(i).ToString();
+                        }
+                    }
+                } while (reader.NextResult());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message + ": " + query);
+                results = e.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return results;
+        }
+    }
+}
