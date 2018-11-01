a00d97c2da795359d8c42ed3058c24d928a53061
ryandorn <43752751+ryandorn@users.noreply.github.com>
2018-11-01 11:18:27 +0000

    Modified Constants file with DB info, added selenium run and config scripts, add run tests script
    Modified constants file with DB server name and DB conneciton name, Added selenium run tests script and runtime and config scripts.

--- ChickenDinnerFL/Framework/Constants.cs
+++ ChickenDinnerFL/Framework/Constants.cs
@@ -9,7 +9,7 @@ namespace ChickenDinnerFL.Framework
     public static class Constants
     {
         private static string REMOTE_WEBDRIVER_ADDRESS = "http://34.219.225.181:4444";
-        public static string DB_CONNECTION_STRING = "@Server=sogetidb.cwffvv6nyyql.us-west-2.rds.amazonaws.com;Databse=TBD;Uid=Hackathon;Pwd=Sogeti999;Connection Timeout=120";
+        public static string DB_CONNECTION_STRING = "@Server=EC2AMAZ-690UJ2P;Databse=AutomationDB;Uid=Hackathon;Pwd=Sogeti999;Connection Timeout=120";
         public static string HOMEPAGE_URL = "https://qa1.hsn.com/";
 
     }
