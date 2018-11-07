7f42cbda6fc3733922154d7074a29b0c6d82cff0
ryandorn <43752751+ryandorn@users.noreply.github.com>
2018-11-07 04:43:10 +0000

    Added packages

--- ChickenDinnerFL/Framework/Constants.cs
+++ ChickenDinnerFL/Framework/Constants.cs
@@ -8,7 +8,7 @@ namespace ChickenDinnerFL.Framework
 {
     public static class Constants
     {
-        private static string REMOTE_WEBDRIVER_ADDRESS = "http://34.219.225.181:4444";
+        public static string REMOTE_WEBDRIVER_ADDRESS = "http://34.219.225.181:4444";
         public static string DB_CONNECTION_STRING = "@Server=EC2AMAZ-690UJ2P;Databse=AutomationDB;Uid=Hackathon;Pwd=Sogeti999;Connection Timeout=120";
         public static string HOMEPAGE_URL = "https://qa1.hsn.com/";
 
